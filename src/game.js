d4550af2124f8fc17e376cbaaafa7a820dd4cb4b
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2022-09-26 15:33:29 -0300

    switching between two different scenes

--- src/game.js
+++ src/game.js
@@ -1,7 +1,8 @@
 // -------------------------------------------------
 // -------------------------------------------------
 
-// import SecondScene from './SecondScene'
+// import SecondScene from './scenes/SecondScene'
+// import BootScene from './scenes/BootScene'
 
 // -------------------------------------------------
 // An initial scene just to load assets
@@ -59,6 +60,7 @@ var WorldScene = new Phaser.Class({
 
   function WorldScene ()
   {
+    console.log(' - WorldScene: constructor');
     Phaser.Scene.call(this, { key: 'WorldScene' });
   },
 
@@ -76,8 +78,8 @@ var WorldScene = new Phaser.Class({
     var tiles = map.addTilesetImage('spritesheet', 'tiles');
 
     // creating the layers
-    var grass = map.createStaticLayer('Grass', tiles, 0, 0);
-    var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+    var grass = map.createLayer('Grass', tiles, 0, 0);
+    var obstacles = map.createLayer('Obstacles', tiles, 0, 0);
 
     // -------------------------------
     // adding background music
@@ -182,7 +184,6 @@ var WorldScene = new Phaser.Class({
     //-------------------------------
 
     // this door makes the game go to a scene 2
-    // this.door = this.physics.add.sprite(472, 150, 'door',0);
     this.door = this.physics.add.sprite(9, 150, 'door',0);
     this.door.setImmovable();
     // this.physics.add.collider(this.player, this.door);
@@ -191,67 +192,10 @@ var WorldScene = new Phaser.Class({
 
   callNewScene: function() {
     console.log(' - Starting the Second Scene');
-    this.cameras.main.shake(300);
-    // this.scene.start('SecondScene');
-    // this.scene.stop().launch('SecondScene');
+    // this.cameras.main.shake(300);
+    // calling the second scene
+    this.scene.start("SecondScene");
   },
-
-  // updatePlayer: function() {
-  // //    this.controls.update(delta);
-  //     this.player.body.setVelocity(0);
-  //
-  //     // Horizontal movement
-  //     if (this.cursors.left.isDown)
-  //     {
-  //         this.player.body.setVelocityX(-80);
-  //     }
-  //     else if (this.cursors.right.isDown)
-  //     {
-  //         this.player.body.setVelocityX(80);
-  //     }
-  //
-  //     // Vertical movement
-  //     if (this.cursors.up.isDown)
-  //     {
-  //         this.player.body.setVelocityY(-80);
-  //     }
-  //     else if (this.cursors.down.isDown)
-  //     {
-  //         this.player.body.setVelocityY(80);
-  //     }
-  //
-  //     // Update the animation last and give left/right animations
-  //     // precedence over up/down animations
-  //     if (this.cursors.left.isDown)
-  //     {
-  //         this.player.anims.play('left', true);
-  //         this.player.flipX = true;
-  //     }
-  //     else if (this.cursors.right.isDown)
-  //     {
-  //         this.player.anims.play('right', true);
-  //         this.player.flipX = false;
-  //     }
-  //     else if (this.cursors.up.isDown)
-  //     {
-  //         this.player.anims.play('up', true);
-  //     }
-  //     else if (this.cursors.down.isDown)
-  //     {
-  //         this.player.anims.play('down', true);
-  //     }
-  //     else
-  //     {
-  //         this.player.anims.stop();
-  //     }
-  // },
-
-  // updateNPCs = function() {
-  //   // making the NPCs to move
-  //   this.npc1.anims.play('walkingMode', true);
-  //   this.npc2.anims.play('walkingMode', true);
-  //   this.npc3.anims.play('walkingMode', true);
-  // },
 
   update: function (time, delta)
   {
@@ -317,62 +261,146 @@ var WorldScene = new Phaser.Class({
 // Second Scene
 // -------------------------------------------------
 
-// var SecondScene = new Phaser.Class({
-//
-//     Extends: Phaser.Scene,
-//
-//     initialize:
-//
-//     function SecondScene ()
-//     {
-//         Phaser.Scene.call(this, { key: 'SecondScene' });
-//         // super('SecondScene');
-//     },
-//
-//     preload: function ()
-//     {
-//
-//     },
-//
-//     create: function ()
-//     {
-//         // create the map
-//         var map = this.make.tilemap({ key: 'map' });
-//
-//         // first parameter is the name of the tilemap in tiled
-//         var tiles = map.addTilesetImage('spritesheet', 'tiles');
-//
-//         // creating the layers
-//         var grass = map.createStaticLayer('Grass', tiles, 0, 0);
-//         var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
-//
-//         // make all tiles in obstacles collidable
-//         obstacles.setCollisionByExclusion([-1]);
-//
-//         // don't go out of the map
-//         this.physics.world.bounds.width = map.widthInPixels;
-//         this.physics.world.bounds.height = map.heightInPixels;
-//         this.player.setCollideWorldBounds(true);
-//
-//         // don't walk on trees
-//         this.physics.add.collider(this.player, obstacles);
-//
-//         //-------------------------------
-//         // Limit camera to map
-//         //-------------------------------
-//
-//         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-//         this.cameras.main.startFollow(this.player);
-//         this.cameras.main.roundPixels = true; // avoid tile bleed
-//
-//         //-------------------------------
-//         // Controlling player with the keyboard
-//         //-------------------------------
-//
-//         // user input
-//         this.cursors = this.input.keyboard.createCursorKeys();
-//     },
-// });
+var SecondScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function SecondScene ()
+    {
+        Phaser.Scene.call(this, { key: 'SecondScene' });
+    },
+
+    preload: function ()
+    {
+
+    },
+
+    create: function ()
+    {
+      // create the map
+      var map = this.make.tilemap({ key: 'map' });
+
+      // first parameter is the name of the tilemap in tiled
+      var tiles = map.addTilesetImage('spritesheet', 'tiles');
+
+      // creating the layers
+      var grass = map.createLayer('Grass', tiles, 0, 0);
+      var obstacles = map.createLayer('Obstacles', tiles, 0, 0);
+
+      // -------------------------------
+      // adding background music
+      // -------------------------------
+      this.music = this.sound.add('backgroundSong');
+      var musicConfig = {
+        mute: false,
+        volume: 1,
+        rate: 1,
+        detune: 0,
+        seek:0,
+        loop: true,
+        delay: 0
+      }
+      this.music.play(musicConfig); // start playing music
+      // -------------------------------
+      // -------------------------------
+
+      // make all tiles in obstacles collidable
+      obstacles.setCollisionByExclusion([-1]);
+
+      // our player sprite created through the phycis system
+      this.player = this.physics.add.sprite(49, 150, 'player', 2);
+
+      // don't go out of the map
+      this.physics.world.bounds.width = map.widthInPixels;
+      this.physics.world.bounds.height = map.heightInPixels;
+      this.player.setCollideWorldBounds(true);
+
+      // don't walk on trees
+      this.physics.add.collider(this.player, obstacles);
+
+      //-------------------------------
+      // Limit camera to map
+      //-------------------------------
+
+      this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+      this.cameras.main.startFollow(this.player);
+      this.cameras.main.roundPixels = true; // avoid tile bleed
+
+      //-------------------------------
+      // Controlling player with the keyboard
+      //-------------------------------
+
+      // user input
+      this.cursors = this.input.keyboard.createCursorKeys();
+
+      //-------------------------------
+      // Adding a door to a new scene
+      //-------------------------------
+
+      // this door makes the game go to a scene 2
+      this.door = this.physics.add.sprite(150, 150, 'door',0);
+      this.door.setImmovable();
+      // this.physics.add.collider(this.player, this.door);
+      this.physics.add.overlap(this.player, this.door, this.callNewScene, false, this);
+    },
+
+    callNewScene: function() {
+      console.log(' - Starting the World Scene');
+      // calling the first scene
+      this.scene.start('WorldScene');
+    },
+
+    update: function (time, delta)
+    {
+      this.player.body.setVelocity(0);
+      // Horizontal movement
+      if (this.cursors.left.isDown)
+      {
+        this.player.body.setVelocityX(-80);
+      }
+      else if (this.cursors.right.isDown)
+      {
+        this.player.body.setVelocityX(80);
+      }
+
+      // Vertical movement
+      if (this.cursors.up.isDown)
+      {
+        this.player.body.setVelocityY(-80);
+      }
+      else if (this.cursors.down.isDown)
+      {
+        this.player.body.setVelocityY(80);
+      }
+
+      // Update the animation last and give left/right animations
+      // precedence over up/down animations
+      if (this.cursors.left.isDown)
+      {
+        this.player.anims.play('left', true);
+        this.player.flipX = true;
+      }
+      else if (this.cursors.right.isDown)
+      {
+        this.player.anims.play('right', true);
+        this.player.flipX = false;
+      }
+      else if (this.cursors.up.isDown)
+      {
+        this.player.anims.play('up', true);
+      }
+      else if (this.cursors.down.isDown)
+      {
+        this.player.anims.play('down', true);
+      }
+      else
+      {
+        this.player.anims.stop();
+      }
+    },
+});
 
 // -------------------------------------------------
 // -------------------------------------------------
@@ -394,8 +422,8 @@ var config = {
   // all the possible scenes
   scene: [
     BootScene,      // just load the asses and call the main scene
-    WorldScene     // the main scene, exploring the map
-    // SecondScene    // a second scene to make a transition
+    WorldScene,     // the main scene, exploring the map
+    SecondScene     // a second scene to make a transition
   ]
 };
 var game = new Phaser.Game(config);
