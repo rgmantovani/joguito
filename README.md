6c78e68f1c0aa2c8809f2aa0ab19b672e224977c
Walison <wpitombeira.ipms@gmail.com>
2022-10-12 19:51:04 -0300

    ## - Improvements in CSS, AutoSize parametrization and readme update

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # joguito
 
-Starting python3 webserver:
+## Starting Development Server
+
+Using python:
 ```
 python -m http.server 9000
 ```
 
+Using PHP ([How to Install PHP](https://www.php.net/manual/en/install.php)):
+```
+PHP -S localhost:9000
+```
+<br>
 and open the game using your browser and typing:
 
 ```
 localhost:9000
 ```
+
+<hr><br>
+
+## Current Folder Format
+<br>
+
+📦 joguito<br>
+ ┣ 📂assets<br>
+ ┃ ┣ 📜Generic image assets which can be used anywhere<br>
+ ┃ ┣ 📂audio<br>
+ ┃ ┃ ┣ 📜 Audios used in project<br>
+ ┃ ┣ 📂map<br>
+ ┃ ┃ ┣ 📜All Map json and TMX files<br>
+ ┣ 📂src<br>
+ ┃ ┣ 📂components<br>
+ ┃ ┃ ┣ 📜Generic but necessary javascript components (such as phaser.js)<br>
+ ┃ ┣ 📂scenes<br>
+ ┃ ┃ ┣ 📜Scene Files<br>
+ ┃ ┣ 📂styles<br>
+ ┃ ┃ ┗ 📜CSS StyleSheets<br>
--- src/game.js
+++ src/game.js
@@ -5,16 +5,35 @@ import BootScene from './scenes/BootScene.js'
 import WorldScene from './scenes/WorldScene.js'
 import SecondScene from './scenes/SecondScene.js'
 import ThirdScene from './scenes/ThirdScene.js'
+import PauseMenu from './scenes/PauseMenu.js'
 
 // -------------------------------------------------
 // -------------------------------------------------
+
+function getZoomValue() {
+  var w = window.innerWidth
+  var h = window.innerHeight
+
+  var zoom = 2;
+
+  switch (true) {
+    case (((w/1920 > 1) && (w/1920 < 2)) && ((h/1280 > 1) && (h/1280 < 2))):
+      zoom = 3
+      break
+    case ((w/1920 > 2) && (h/1280 > 2)):
+      zoom = 4
+      break
+  }
+  
+  return zoom
+}
 
 var config = {
   type: Phaser.AUTO,
   parent: 'content',
-  width: 640,
-  height: 480,
-  zoom: 1,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  zoom: getZoomValue(),
   pixelArt: true,
   physics: {
     default: 'arcade',
@@ -29,8 +48,10 @@ var config = {
     WorldScene,     // the main scene, exploring the map
     SecondScene,    // a second scene to make a transition
     ThirdScene,     // a second scene to make a transition  
+    PauseMenu,      // the pause menu with basic functionalities
   ]
 };
+
 var game = new Phaser.Game(config);
 
 // -------------------------------------------------
